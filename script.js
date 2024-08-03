c3f76cd70d55c10b738c1c26d6c0d532b11b401f
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-08-03 17:02:44 +1200

    adding the name of the item and the add to cart button

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ var swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev'
   }
 })
+
+
+function addToCart (itemName) {
+  // Logic to add the item to the cart
+  alert(itemName + ' has been added to the cart!')
+  // You can replace the alert with actual cart functionality
+}
