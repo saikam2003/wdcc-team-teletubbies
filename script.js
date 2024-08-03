971c542e6cd7adb16656d51cdf119dc720e70744
saikam2003 <126769938+saikam2003@users.noreply.github.com>
2024-08-03 18:31:35 +1200

    added disappearing navbar

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+// var swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev'
+//   }
+// })
+
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
+  effect: 'cards',
+    grabCursor: true,
+    loop: true,
+  
 })
+
+let lastScrollTop = 0 // Variable to track last scroll position
+const navbar = document.querySelector('.navbar') // Get the navbar element
+
+window.addEventListener('scroll', function () {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    navbar.classList.add('hidden') // Add the hidden class to the navbar
+  } else {
+    // Scrolling up
+    navbar.classList.remove('hidden') // Remove the hidden class from the navbar
+  }
+  lastScrollTop = scrollTop // Update the last scroll position
+})
+
 
 
 function addToCart (itemName) {
